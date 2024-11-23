c05e40c3dcfaff74f83c07afb8810a802d17f655
11-sg <sg4242674@gmail.com>
2024-11-24 01:36:37 +0530

    update

---
+++ script.js
@@ -0,0 +1,8 @@
+const signbtn=document.querySelector(".btn-primary");
+signbtn.addEventListener("click", function(){
+    alert("Sign Up button click");
+})
+
+document.querySelector(".btn-secondary").addEventListener("click", function(){
+    alert("Book demo button clicked!");
+})
